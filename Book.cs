b544f5cbcebc908e6d3098f7ffa34ac47e4af48d
3685-Sr <svetliradnev@gmail.com>
2025-04-05 11:26:37 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_3
+{
+    public class Book
+    {   
+        //fields - полета
+        public string Title;
+        public string Author;
+        public int Pages;
+        public double Price;
+
+        //constructor - конструктор
+        public Book(string title, string author, int pages, double price)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Price = price;
+        }
+
+        public void PrintBookData()
+        {
+            Console.WriteLine("Title:" + Title);
+            Console.WriteLine("Author:" + Author);
+            Console.WriteLine("Pages:" + Pages);
+            Console.WriteLine("Price:" + Price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Under the yoke", "Ivan Vazov", 215, 18.50);
+            Book book2 = new Book("Brothers Karamazov", "Fedor Dostoevskiy", 818, 35.99);
+            Book book3 = new Book("Future", "Dimitriy Gluhovkiy", 389, 25.90);
+
+            book1.PrintBookData();
+            Console.WriteLine("============");
+            book2.PrintBookData();
+            Console.WriteLine("============");
+            book3.PrintBookData();
+            Console.WriteLine("============");
+        }
+
+    }
+}
